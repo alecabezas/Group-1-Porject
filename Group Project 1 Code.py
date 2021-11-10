2ea84795f4f96014e1712c138ce47d83d7ace126
alecabezas <94082817+alecabezas@users.noreply.github.com>
2021-11-10 17:33:46 -0500

    Add files via upload

---
+++ Group Project 1 Code.py
@@ -0,0 +1,41 @@
+users = {'Alex': ['taylor swift', 'hannah montana', 'juice world']
+         'Skye': ['hannah montana', 'childish gambino', 'the beetles']
+         'Luke': ['pop smoke', 'hannah montana', 'crumb', 'acdc']}
+likes = {}
+def isPrivate(user):
+    if user[-1] == '$':
+        return true
+    else:
+        return false
+
+def dictionaryOfLikes(users):
+    
+         
+
+def showMostPopularArtist(users):
+    '''displays the most popular artists
+    1. see if the user is in private mode, if so iterate to next user
+    2. iterate through each non private user and create a new dictionary with each new artist
+    3. update the dictionary value if the artist is found in the dictionary
+    4. iterate trough the dictionary and return the name of the three highest values
+    maybe use memoization'''
+
+def howPopularIsTheMostPopular(users):
+    '''displays the amount of likes of the most popular artist
+    1. similar code to showMOstPopular artist, maybe make the iterating and comparing a helper function
+    2. return the maximum value of likes from the dictionary through recursion
+    maybe use memoization?? '''
+
+def whichUserLikesTheMostArtists(users):
+    ''' gets the user with the most likes (most inputted artist)
+    1. see if user or next user is private
+    2. if not compare user one and two with two iterating loops (i and j)
+    3. if only one user next, compare that user '''
+
+'''helper function that calculates the number of likes for each artist'''
+
+'''questions I haven't figured out:
+    What is the best way to compare the lists?
+    What syntax do I use to access the file and the values inside?
+    Is there
+    '''
